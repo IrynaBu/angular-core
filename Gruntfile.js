dec47bd4875595fe0e17c69df3e33a33829e17bc
Irina Budko <irina.budko@intexsoft.by>
2016-01-11 11:47:37 +0300

    fix name building app

--- Gruntfile.js
+++ Gruntfile.js
@@ -403,7 +403,7 @@ module.exports = function (grunt) {
 		ngtemplates: {
 			dist: {
 				options: {
-					module: 'yyyApp',
+					module: 'yoNewAngularApp',
 					htmlmin: '<%= htmlmin.dist.options %>',
 					usemin: 'scripts/scripts.js'
 				},
